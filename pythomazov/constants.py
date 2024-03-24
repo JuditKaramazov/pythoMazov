23d7693b38d739f1111cecdbd233c54840825f74
JuditKaramazov <j.lazaromoyano7@gmail.com>
2024-03-24 19:40:19 +0100

    Add constants.py

---
+++ pythomazov/constants.py
@@ -0,0 +1,39 @@
+# Common
+GOOGLE_TAG = "GTM-TGWFR79L"
+
+# Header
+GITHUB_URL = "https://github.com/JuditKaramazovv"
+LINKEDIN_URL = "https://www.linkedin.com/in/juditlazaromoyano/"
+TWITTER_X_URL = "https://twitter.com/V_Karamazov"
+
+# Courses & Material
+PYTHON_INTRODUCTION_COURSE_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/03-Courses/01-Python/README.md"
+GIT_COURSE_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/03-Courses/02-Git-GitHub/README.md"
+CERTIFICATES_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/04-Certificates/README.md"
+PYTHON_CHEATSHEETS_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/05-Cheatsheets/01-Python/README.md"
+GIT_CHEATSHEETS_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/05-Cheatsheets/02-Git/README.md"
+IDE_INTRODUCTION_COURSE_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/06-IDEs/README.md"
+GAME_DEVELOPMENT_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/07-Game-development/README.md"
+
+# Practice
+CODEDEX_URL="https://codedex.io/"
+CODEWARS_URL="https://www.codewars.com/"
+CODINGAME_URL="https://www.codingame.com/start/"
+EXERCISM_URL="https://exercism.io/"
+OHMYGIT_URL="https://ohmygit.org"
+WARRIORJS_URL="https://warriorjs.com/"
+
+# Resources
+BOOKS_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/01-Books/README.md"
+SITES_URL = "https://github.com/JuditKaramazov/InsightsFromJuniorToFutureSeniors/blob/main/content/02-Websites/README.md"
+SETUP_URL = "https://karamablog.xyz/tecnomazov"
+REPO_URL = "https://github.com/JuditKaramazov/pythoMazov"
+
+# Personal sites
+KARAMABLOG_URL = "https://karamablog.xyz"
+KARAMAZFOLIO_URL = "https://karamazfolio.xyz/"
+
+# Contact & Support
+COFFEE_URL = "https://buymeacoffee.com/JuditKaramazov"
+EMAIL = "j.lazaromoyano7@gmail.com"
+LINKTREE_URL = "https://linktr.ee/JuditKaramazov"
