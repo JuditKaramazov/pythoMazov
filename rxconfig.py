de059d1475615c89da9b913c66129291975003e6
JuditKaramazov <j.lazaromoyano7@gmail.com>
2024-03-24 19:36:40 +0100

    Add rxconfig.py

---
+++ rxconfig.py
@@ -0,0 +1,9 @@
+import reflex as rx
+
+config = rx.Config(
+    app_name="pythomazov",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://pythomazov.vercel.app"
+    ]
+)
