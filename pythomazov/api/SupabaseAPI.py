091ac9623e6cb7a1c485d19fc4deeb287ec6ccb7
JuditKaramazov <j.lazaromoyano7@gmail.com>
2024-03-24 19:40:50 +0100

    Create SupabaseAPI.py

---
+++ pythomazov/api/SupabaseAPI.py
@@ -0,0 +1,36 @@
+import os
+import dotenv
+from supabase import create_client, Client
+from pythomazov.model.Featured import Featured
+
+
+class SupabaseAPI:
+
+    dotenv.load_dotenv()
+
+    SUPABASE_URL = os.environ.get("SUPABASE_URL")
+    SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+    def __init__(self) -> None:
+        if self.SUPABASE_URL != None and self.SUPABASE_KEY != None:
+            self.supabase: Client = create_client(
+                self.SUPABASE_URL, self.SUPABASE_KEY
+            )
+
+    def featured(self) -> list[Featured]:
+
+        response = self.supabase.table("featured").select("*").execute()
+
+        featured_data = []
+
+        if len(response.data) > 0:
+            for featured_item in response.data:
+                featured_data.append(
+                    Featured(
+                        title=featured_item["title"],
+                        image=featured_item["image"],
+                        url=featured_item["url"]
+                    )
+                )
+
+        return featured_data
