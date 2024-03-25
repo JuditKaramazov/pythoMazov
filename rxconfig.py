0087b8fc8a0769fe9dcba7dab1219fbd2b6321df
JuditKaramazov <j.lazaromoyano7@gmail.com>
2024-03-25 15:39:22 +0100

    Redefine config

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="pythomazov",
-    cors_allowed_origins=[
-        "http://localhost:3000",
-        "https://pythomazov.reflex.run",
-        "https://www.pythomazov.tech",
-        "https://pythomazov.tech"
-    ]
+    app_name="pythomazov"
 )
